0836bae14704c90687bd4a15e0ed6dd411fa5d9e
shimant01 <shimantoreza01@gmail.com>
2025-05-23 06:08:19 +0600

    Add files via upload

---
+++ chat_app/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chat app</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+</head>
+<?php include_once "header.php"; ?>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>chatapp</header>
+            <form action="#" method="post" enctype="multipart/form-data" autocomplete="off">
+                <div class="errortext"> </div>
+                <div class="name-details">
+                    <div class="field input">
+                        <label >First Name</label>
+                        <input type="text" name="fname" placeholder="First Name" required>
+                    </div>
+                    <div class="field input">
+                        <label >Last Name</label>
+                        <input type="text" name="lname" placeholder="Last Name" required>
+                    </div>
+
+                </div>
+                <div class="field input">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="Enter your Email" required>
+                </div>
+                     <div class="field input">
+                        <label >Password</label>
+                        <input type="password" name="password" placeholder="Enter New Password" required>
+                <i class="fas fa-eye"></i>
+                
+                </div>
+                     <div class="field image">
+                        <label >Profile Image</label>
+                        <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg">
+                </div>
+                 
+                   <div class="field button">
+                      <input type="submit" name="submit" value="Continue to Chat" required>
+                     
+                 
+                    </div>
+                
+
+
+            </form>
+            <div class="link">
+                Already signed up ? <a href="login.php">Login now</a>
+            </div>
+        </section>
+    </div>
+
+
+<script type="text/javascript" src="js/pass-show-hide.js"></script>
+<script type="text/javascript" src="js/signup.js"></script>
+
+
+
+</body>
+
+</html>
---
+++ chat_app/php/login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+include_once "config.php";
+
+$email    = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+
+if (!empty($email) && !empty($password)) {
+
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+
+    if (mysqli_num_rows($sql) > 0) {
+
+        $row       = mysqli_fetch_assoc($sql);
+        $user_pass = md5($password);
+        $enc_pass  = $row['password'];
+
+        if ($user_pass === $enc_pass) {
+
+            $status = "Online";
+            //  Correct way to use the variable:  Remove the single quotes around $unique_id
+            $sql2 = mysqli_query(
+                $conn,
+                "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}"
+            );
+
+            if ($sql2) {
+                $_SESSION['unique_id'] = $row['unique_id'];
+                echo "success";
+            } else {
+                echo "Something went wrong .please try again!";
+            }
+
+        } else {
+            echo "Email or Password is Incorrect!";
+        }
+
+    } else {
+        echo "$email - This email not exist!";
+    }
+
+} else {
+    echo "All input fields are required!";
+}
+?>
